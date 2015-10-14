4094280cebe85921dd52fd2b1ab4b171edaec7da
angryllama <ducky800@gmail.com>
2015-10-14 15:22:49 -0700

    End of lesson 2

--- app/src/main/java/com/example/dan/justjava/MainActivity.java
+++ app/src/main/java/com/example/dan/justjava/MainActivity.java
@@ -20,12 +20,17 @@ public class MainActivity extends AppCompatActivity {
     // Variable for number of Coffees
     int quantity = 0;
 
+
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+        //display(quantity);
+        //displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Amount Due $" + price + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -43,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     // Method for increasing quantity

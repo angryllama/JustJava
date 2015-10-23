8ea52885a247dbcecd9705b4c9bf093a5cd37808
angryllama <ducky800@gmail.com>
2015-10-23 11:38:53 -0700

    Basically complete

--- app/src/main/java/com/example/dan/justjava/MainActivity.java
+++ app/src/main/java/com/example/dan/justjava/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.dan.justjava;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variable for number of Coffees
+    int quantity = 0;
+    // Variable for total price of order
+    int price = 0;
+
+    // Creates Checkboxes and booleans to store whether whipped cream or chocolate is selected
+    CheckBox isWhippedBox;
+    CheckBox isChocolateBox;
+    boolean isWhipped;
+    boolean isChocolate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,35 +36,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Variable for number of Coffees
-    int quantity = 0;
-    // Variable for total price of order
-    int price = 0;
-    // Boolean for whipped cream
-    //boolean isWhipped = false;
-    // Boolean for whipped cream
-    //boolean isChocolate = false;
-
-
     /**
      * This method is called when to calculate price of order
      */
     public int calculatePrice() {
-        return quantity * 5;
+        // Init base price
+        int price = 5;
+
+        // If Whipped cream added, add $1
+        if (isWhipped) price += 1;
+
+        // If Chocolate added, add $2
+        if (isChocolate) price += 2;
+
+        // Calculate and return final price
+        return quantity * price;
     }
+
 
     /**
      * This method is called to create the text of the order summary
      */
     public String createSummary(int price) {
 
-        CheckBox isWhippedBox = (CheckBox) findViewById(R.id.boxWhipped);
-        boolean isWhipped = isWhippedBox.isChecked();
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+        String name = nameField.getText().toString();
 
-        CheckBox isChocolateBox = (CheckBox) findViewById(R.id.boxChocolate);
-        boolean isChocolate = isChocolateBox.isChecked();
-
-        return "Name: Kunal" +
+        return "Name: " + name +
                 "\nAdd Whipped Cream?: " + isWhipped +
                 "\nAdd Chocolate?: " + isChocolate +
                 "\nQuantity: " + quantity +
@@ -61,9 +76,48 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         //display(quantity);
         //displayPrice(quantity * 5);
+
+        // Assign and store whipped cream checkbox and boolean, checks whether whipped cream is
+        // ordered and stores it.
+        isWhippedBox = (CheckBox) findViewById(R.id.boxWhipped);
+        isWhipped = isWhippedBox.isChecked();
+
+        // Assign and store choco checkbox and boolean, checks whether choco is
+        // ordered and stores it.
+        isChocolateBox = (CheckBox) findViewById(R.id.boxChocolate);
+        isChocolate = isChocolateBox.isChecked();
+
         price = calculatePrice();
         displayMessage(createSummary(price));
     }
+
+    /**
+     * This method is called when the email order button is clicked.
+     * Instead of displaying the order, it sends the order through email
+     */
+    public void submitOrderEmail(View view) {
+
+        // Assign and store whipped cream checkbox and boolean, checks whether whipped cream is
+        // ordered and stores it.
+        isWhippedBox = (CheckBox) findViewById(R.id.boxWhipped);
+        isWhipped = isWhippedBox.isChecked();
+
+        // Assign and store choco checkbox and boolean, checks whether choco is
+        // ordered and stores it.
+        isChocolateBox = (CheckBox) findViewById(R.id.boxChocolate);
+        isChocolate = isChocolateBox.isChecked();
+
+        price = calculatePrice();
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order");
+        intent.putExtra(Intent.EXTRA_TEXT, createSummary(price));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -92,15 +146,24 @@ public class MainActivity extends AppCompatActivity {
 
     // Method for increasing quantity
     public void quantityIncrease(View view) {
-        quantity++;
-        display(quantity);
+        if (quantity <= 99) {
+            quantity++;
+            display(quantity);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Can't order more than 100!", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
+
 
     // Method for increasing quantity
     public void quantityDecrease(View view) {
         if (quantity >= 1) {
             quantity--;
             display(quantity);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "Can't order negative coffees!", Toast.LENGTH_LONG);
+            toast.show();
         }
     }
 
